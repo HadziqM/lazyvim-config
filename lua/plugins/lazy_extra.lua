98ec7ae0e5c08904ffd1f964da681b820b20dac7
HadziqM <dimascrazz@gmail.com>
2024-12-01 00:42:36 +0700

    syncing

---
+++ lua/plugins/lazy_extra.lua
@@ -0,0 +1,10 @@
+local T = {
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
+  { import = "lazyvim.plugins.extras.coding.codeium" },
+  { import = "lazyvim.plugins.extras.coding.mini-surround" },
+  { import = "lazyvim.plugins.extras.lang.sql" },
+  { import = "lazyvim.plugins.extras.lang.svelte" },
+}
+
+return T
